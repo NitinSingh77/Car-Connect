7a5ccc00b4574a0cbcc77f6a60f82685f439f7b8
NitinSingh <69206166+NitinSingh77@users.noreply.github.com>
2024-10-07 16:53:56 +0530

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,29 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/**
- * 
- */
 /**
  * 
  */
 module CarConnect {
 	requires java.sql;
-=======
-/**
- * 
- */
-/**
- * 
- */
-module CarConnect {
->>>>>>> cf09df744e197d6953e9e83a50eb81514f430173
-=======
-/**
- * 
- */
-/**
- * 
- */
-module CarConnect {
->>>>>>> cf09df744e197d6953e9e83a50eb81514f430173
 }
