4a54fac2860a9a82590136b26009aafb88482e0c
Meghna Gupta <108588561+meghnagupta-2002@users.noreply.github.com>
2024-10-07 18:15:46 +0530

    IAdminService.java

--- src/carConnect/dao/IAdminService.java
+++ src/carConnect/dao/IAdminService.java
@@ -4,7 +4,7 @@ import carConnect.entity.Admin;
 
 public interface IAdminService {
 	//Declaration of Non-Implemented Methods
-	Admin getAdminById(int adminId);
+	Admin getAdminByID(int adminID);
     Admin getAdminByUsername(String username);
     void registerAdmin(Admin admin);
     void updateAdmin(Admin admin);
