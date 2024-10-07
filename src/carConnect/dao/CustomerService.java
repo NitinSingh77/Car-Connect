ecdabd2ed3e726bb6b1aa4321b5918e496c63d97
NitinSingh <69206166+NitinSingh77@users.noreply.github.com>
2024-10-07 22:36:24 +0530

    Update CustomerService.java

--- src/carConnect/dao/CustomerService.java
+++ src/carConnect/dao/CustomerService.java
@@ -17,13 +17,48 @@ public class CustomerService implements ICustomerService{
 	@Override
 	public Customer getCustomerById(int customerId) {
 		// TODO Auto-generated method stub
-		return null;
+		getCon();
+		Customer obj= null;
+		PreparedStatement pstmt= null;
+		try {
+			pstmt= con.prepareStatement("Select * from Customer where customerId=?");
+			pstmt.setInt(1, customerId);
+			ResultSet res= pstmt.executeQuery();
+			
+			if(res.next())
+			{
+				 obj= new Customer(res.getString("FirstName"), res.getString("LastName"), res.getString("Email"), res.getString("PhoneNumber"), res.getString("Address"), res.getString("Username"), res.getString("Password"));
+			}
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			System.out.println("Error while finding Customer details!");
+		}
+		return obj;
 	}
 
 	@Override
 	public Customer getCustomerByUsername(String username) {
 		// TODO Auto-generated method stub
-		return null;
+		getCon();
+		Customer obj= null;
+		PreparedStatement pstmt= null;
+		try {
+			pstmt= con.prepareStatement("Select * from Customer where username=?");
+			pstmt.setString(1, username);
+			ResultSet res= pstmt.executeQuery();
+			
+			if(res.next())
+			{
+				 obj= new Customer(res.getString("FirstName"), res.getString("LastName"), res.getString("Email"), res.getString("PhoneNumber"), res.getString("Address"), res.getString("Username"), res.getString("Password"));
+			}
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			System.out.println("Error while finding Customer details!");
+		}
+		return obj;
+		
 	}
 
 	@Override
@@ -58,12 +93,54 @@ public class CustomerService implements ICustomerService{
 	@Override
 	public void updateCustomer(Customer customerData) {
 		// TODO Auto-generated method stub
+		getCon();
 		
+		PreparedStatement pstmt= null;
+		try {
+			pstmt= con.prepareStatement("update Customer set FirstName=?, LastName=?, Email=?, PhoneNumber=?, Address=?, Username=?, Password=?");
+			pstmt.setString(1, customerData.getFirstName()); 
+		    pstmt.setString(2, customerData.getLastName());
+		    pstmt.setString(3, customerData.getEmail());
+		    pstmt.setString(4, customerData.getPhoneNumber());
+		    pstmt.setString(5, customerData.getAddress());
+		    pstmt.setString(6, customerData.getUsername());
+		    pstmt.setString(7, customerData.getPassword());
+		    
+		    int roweffected= pstmt.executeUpdate();
+		    if(roweffected>0)
+		    {
+		    	System.out.println("Row updated successfully!");
+		    }
+		    
+			
+	}catch(SQLException e)
+		{
+		e.printStackTrace();
+		System.out.println("Error while Updating in Customer table!");
+		}
 	}
 
 	@Override
 	public void deleteCustomer(int customerId) {
 		// TODO Auto-generated method stub
+		getCon();
+		
+		PreparedStatement pstmt= null;
+		try {
+			pstmt= con.prepareStatement("delete from Customer where customerId=?");
+			pstmt.setInt(1, customerId); 		    
+		    int roweffected= pstmt.executeUpdate();
+		    if(roweffected>0)
+		    {
+		    	System.out.println("Row Deleted successfully!");
+		    }
+		    
+			
+	}catch(SQLException e)
+		{
+		e.printStackTrace();
+		System.out.println("Error while deleting in Customer table!");
+		}
 		
 	}
 	
